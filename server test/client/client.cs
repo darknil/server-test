cdba20c78633bbf5186e0bfb7387d4f5dc224319
alex <45723342+darknil@users.noreply.github.com>
2020-12-02 13:21:10 +0300

    Add files via upload

---
+++ server test/client/client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    class client
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            try
+            {
+                client.Connect("127.0.0.1",8080);
+                Console.WriteLine("connected");
+                Console.ReadLine();
+                string message = "hi server";
+                byte[] buffer = Encoding.ASCII.GetBytes(message);
+                client.Send(buffer);
+
+                byte[] buffer2 = new byte[1024];
+                client.Receive(buffer2);
+                Console.WriteLine(Encoding.ASCII.GetString(buffer2));
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ server test/server test/server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server_test
+{
+    class server
+    {
+
+      
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            try
+            {
+                server.Bind(ipPoint);
+                server.Listen(5);
+                Console.WriteLine("server started...");
+                Socket client = server.Accept();
+                byte[] buffer = new byte[1024];
+                client.Receive(buffer);
+                Console.WriteLine(Encoding.ASCII.GetString(buffer));
+                Console.WriteLine("напиши ответ");
+                string message = Console.ReadLine();
+                byte[] buffer2 = Encoding.ASCII.GetBytes(message);
+                client.Send(buffer2);
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
